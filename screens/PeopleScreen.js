9815af75a0e3b3e797cc5e4eaa101be8f2e674ac
Rocien Nkunga <rociennkunga@gmail.com>
2024-10-11 02:45:43 -0400

    added the styling on peopleScreen and text

--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -20,21 +20,27 @@ export default function PeopleScreen() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <FlatList
-          data={people}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.name}</Text>
-              <Text>{item.dob}</Text>
-            </View>
-          )}
-        />
+        <Text style={styles.title}>People List</Text>
+
+        {people.length === 0 ? (
+          <Text style={styles.savedText}>No People Saved Yet.</Text>
+        ) : (
+          <FlatList
+            data={people}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item.name}</Text>
+                <Text>{item.dob}</Text>
+              </View>
+            )}
+          />
+        )}
+
         {/* Floating Action Button */}
         <TouchableOpacity style={styles.fab} onPress={() => navigation.navigate('AddPerson')}>
           <MaterialIcons name="add" size={60} color="white" />
         </TouchableOpacity>
-        {/* <Button title="Add Person" onPress={() => navigation.navigate('AddPerson')} /> */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -42,19 +48,32 @@ export default function PeopleScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    borderTopWidth: 2,
+    borderColor: '#ddd',
+    padding: 20
+  },
+  title: {
+    fontSize: 40,
+    paddingTop: 20,
+    paddingLeft: 20,
+    textAlign: 'center'
+  },
+  savedText: {
+    textAlign: 'center',
+    color: '#606060',
+    padding: 10,
+    fontSize: 20
   },
   fab: {
     position: 'absolute',
-    bottom: 50,
+    bottom: 100,
     left: '50%',
     transform: [{ translateX: -30 }],
     backgroundColor: 'green',
     borderRadius: 30,
     width: 60,
     height: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
     elevation: 5
   }
 });
